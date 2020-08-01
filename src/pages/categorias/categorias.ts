49eb7603d2435ebb31ffd239961ab3f27fb5a80a
Adriano Luis Machado Abreu <adriano.luis.machado2013@hormail.com>
2020-08-01 12:11:51 -0300

    Criando um interceptor para tratamento de erros

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -31,9 +31,7 @@ export class CategoriasPage {
         console.log(response);
         this.items = response;
       },
-      error => {
-        console.log(error);
-      });
+      error => {});
   }
 
 }
