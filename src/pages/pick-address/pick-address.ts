39d59d671ec5ef53d53326a42bc70fd1d1af36a6
Adriano Luis Machado Abreu <adriano.luis.machado2013@hormail.com>
2020-09-07 13:02:27 -0300

    Armazenamento dos dados do pedido

--- src/pages/pick-address/pick-address.ts
+++ src/pages/pick-address/pick-address.ts
@@ -3,6 +3,8 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EnderecoDTO } from '../../models/endereco.dto';
 import { StorageService } from '../../services/storage.service';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { PedidoDTO } from '../../models/pedido.dto';
+import { CartService } from '../../services/domain/cart.service';
 
 /**
  * Generated class for the PickAddressPage page.
@@ -19,8 +21,9 @@ import { ClienteService } from '../../services/domain/cliente.service';
 export class PickAddressPage {
 
   items: EnderecoDTO[];
+  pedido: PedidoDTO;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: StorageService, public clienteService: ClienteService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public storage: StorageService, public clienteService: ClienteService, public cartService: CartService) {
   }
 
   ionViewDidLoad() {
@@ -29,6 +32,14 @@ export class PickAddressPage {
       this.clienteService.findByEmail(localUser.email)
         .subscribe(response => {
           this.items = response['enderecos'];
+          let cart = this.cartService.getCart();
+
+          this.pedido = {
+            cliente: {id: response['id']},
+            enderecoDeEntrega: null,
+            pagamento: null,
+            itens : cart.items.map(x => {return {quantidade: x.quantidade, produto: {id: x.produto.id}}})
+          }
         },
         error => {
           if (error.status == 403) {
@@ -39,6 +50,11 @@ export class PickAddressPage {
     else {
       this.navCtrl.setRoot('HomePage');
     }
+  }
+
+  nextPage(item: EnderecoDTO) {
+    this.pedido.enderecoDeEntrega = {id: item.id};
+    console.log(this.pedido); 
   }
  }
 
